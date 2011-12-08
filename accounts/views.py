99891a013d583e51750c86d5724b90168cb9715a
mark_march <markofmarch@gmail.com>
2011-12-08 17:37:08 -0500

    reorginized views and add profile view

--- accounts/views.py
+++ accounts/views.py
@@ -6,7 +6,7 @@ from django.template import RequestContext
 from django.shortcuts import render_to_response
 from django.contrib.messages.api import get_messages
 from django.core.context_processors import csrf
-
+import urllib, hashlib
 from django.contrib.auth.models import User
 
 from forms import *
@@ -23,21 +23,67 @@ def signin(request):
             if user is not None:
                 if user.is_active:
                     login(request, user)
-                    return HttpResponseRedirect("/home")
+                    return HttpResponseRedirect("/")
                 else:
-                    return HttpResponseRedirect("")
+                    return HttpResponseRedirect("/")
             else:
                 errors = form._errors.setdefault(forms.forms.NON_FIELD_ERRORS, forms.util.ErrorList())
                 errors.append(u"Invalid username or password")
-        return render_to_response('signin.html', {"form": form}, 
+        return render_to_response('accounts/signin.html', {"form": form}, 
             context_instance=RequestContext(request))
     else:
-        return render_to_response('signin.html', Request_instance=RequestContext(request))
-    
+        form = SignInForm()
+        return render_to_response('accounts/signin.html', {"form": form},
+            context_instance=RequestContext(request))
 
-def home(request):
-    """Home view, displays login mechanism"""
-    return render_to_response('home.html',RequestContext(request))
+@login_required(login_url='/accounts/signin/')
+def profile(request):
+    """User Profile page"""
+    user = request.user
+    profile = user.profile
+    default = "http://www.gravatar.com/avatar/"
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = request.user
+            user.email = cd['email']
+            gravatar = "http://www.gravatar.com/avatar/"+hashlib.md5(user.email.lower()).hexdigest()+"?"
+            gravatar += urllib.urlencode({'d': default, 's':str(40)})
+            user.profile.gravatar = gravatar
+            user.first_name = cd['first_name']
+            user.last_name = cd['last_name']
+            user.profile.website = cd['website']
+            user.profile.twitter = cd['twitter']
+            user.profile.github = cd['github']
+            user.save()
+            user.profile.save()
+            return render_to_response("accounts/profile.html", 
+                {"form": form, "message": u'Updated success', "gravatar":gravatar}, 
+                context_instance=RequestContext(request))
+        else:
+            return render_to_response("accounts/profile.html", {"form": form}, 
+                context_instance=RequestContext(request))
+    else:
+        gravatar = "http://www.gravatar.com/avatar/"+hashlib.md5(user.email.lower()).hexdigest()+"?"
+        gravatar += urllib.urlencode({'d': default, 's':str(40)})
+        user.profile.gravatar = gravatar
+        user.profile.save()
+        form = UserProfileForm(initial={
+            'username': user.username,
+            'email': user.email,
+            'first_name': user.first_name,
+            'last_name': user.last_name,
+            'website': user.profile.website,
+            'twitter': user.profile.twitter,
+            'github': user.profile.github})
+        return render_to_response("accounts/profile.html", {"form": form}, 
+                context_instance=RequestContext(request)) 
+
+
+def dashboard(request):
+    """Home view,"""
+    return render_to_response('dashboard.html',RequestContext(request))
 
 def signup(request):
     """Sign up view, display sign up page"""
@@ -51,27 +97,20 @@ def signup(request):
             if user is not None:
                 user = authenticate(username=cd['username'], password=cd['password'])
                 login(request, user)
-                return HttpResponseRedirect("/account/edit/")
+                return HttpResponseRedirect("/accounts/edit/")
             else:
-                return HttpResponseRedirect("/signup/")
+                return HttpResponseRedirect("/accounts/signup/")
     else:
         form = SignUpForm()
-    return render_to_response('signup.html', {
+    return render_to_response('accounts/signup.html', {
         'form': form
     }, context_instance=RequestContext(request))
-
-@login_required
-def done(request):
-    """Login complete view, displays user data"""
-    return render_to_response('home.html', RequestContext(request))
-
-def error(request):
-    """Error view"""
-    messages = get_messages(request)
-    return render_to_response('error.html', {'messages': messages},
-                              RequestContext(request))
 
 def signout(request):
     """Logs out user"""
     auth_logout(request)
     return HttpResponseRedirect('/')
+
+def about(request):
+    """ About page view """
+    return render_to_response('about.html', context_instance=RequestContext(request))
