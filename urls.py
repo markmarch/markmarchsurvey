9f99f7ea7ab6394bcef752b5f66c3a2eddf26284
mark_march <markofmarch@gmail.com>
2011-12-08 17:40:46 -0500

    changed url patterns

--- urls.py
+++ urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls.defaults import *
 import settings
 from accounts.views import *
+from survey.views import *
 
 handler500 = 'djangotoolbox.errorviews.server_error'
 
@@ -10,10 +11,13 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', home, name='home'),
-    url(r'^signin/$', signin, name='signin'),
-    url(r'^signup/$', signup, name='signup'),
-    url(r'^home/$', home, name='home'),
-    url(r'^signout/$', signout, name='signout'),
+    url(r'^home/', home, name='home'),
+    url(r'^accounts/signin/$', signin, name='signin'),
+    url(r'^accounts/signup/$', signup, name='signup'),
+    url(r'^accounts/signout/$', signout, name='signout'),
+    url(r'^accounts/profile/$', profile, name='profile'),
+    url(r'^dashboard/$', dashboard, name='dashboard'),
+    url(r'^about/$', about, name='about'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('social_auth.urls')),
 ) 
