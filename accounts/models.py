a4e7a92db8875ac2754eeeedf7bd2e9a6133a0de
mark_march <markofmarch@gmail.com>
2011-12-08 17:36:27 -0500

    added UserProfile model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django import forms
 
-# Create your models here.
+class UserProfile(models.Model):
+    user = models.ForeignKey(User, unique=True)
+    website = models.URLField("Website", blank=True)
+    gravatar = models.URLField("Gravatar", blank=True)
+    twitter = models.CharField("Twitter", max_length=30)
+    github = models.CharField("Github", max_length=30)
+
+User.profile = property(lambda u: UserProfile.objects.get_or_create(user=u)[0])
