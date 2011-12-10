c9cd0dca7ff504e657c2beb95703c253bca7b551
mark_march <markofmarch@gmail.com>
2011-12-10 01:07:08 -0500

    added survey models to show up at admin page

---
+++ survey/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from survey.models import *
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+class PollAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+
+class PollInline(admin.StackedInline):
+    model = Poll
+    extra = 3
+
+class SurveyAdmin(admin.ModelAdmin):
+    inlines = [PollInline]
+
+admin.site.register(Survey, SurveyAdmin)
+admin.site.register(Poll, PollAdmin)
