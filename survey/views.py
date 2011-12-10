7f4b129968cf92afc0083d6c49fd9008ac4adcf7
mark_march <markofmarch@gmail.com>
2011-12-10 01:08:04 -0500

    added home view

--- survey/views.py
+++ survey/views.py
@@ -1,8 +1,19 @@
 from django.http import HttpResponseRedirect
 from django.template import RequestContext
 from django.shortcuts import render_to_response
+from survey.models import *
 
 def home(request):
     """Home View, displays public surveys if user not logged in,
     otherwise display public and private surveys"""
-    return render_to_response('survey/home.html', context_instance=RequestContext(request))
+    user = request.user
+    public_surveys = Survey.objects.filter(visibility='public')
+    if user.is_authenticated:
+        user_surveys = Survey.objects.filter(user__username=user.username)
+        return render_to_response('survey/home.html', 
+            {"user_surveys": user_surveys,
+             "public_surveys": public_surveys},
+            context_instance=RequestContext(request))
+    return render_to_response('survey/home.html', 
+        {"public_surveys": public_survey},
+        context_instance=RequestContext(request))
